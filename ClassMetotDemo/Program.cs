b2149e390026e1094f0cd38127cef7019ff93d26
Eren <77566770+Erkoo-del@users.noreply.github.com>
2021-01-21 22:22:28 +0300

    Add files via upload

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Musteri musteri1 = new Musteri();
+                musteri1.Id = 1;
+                musteri1.isim = "Eren ";
+                musteri1.soyisim = "Sabah";
+                musteri1.yas = 30;
+
+                Musteri musteri2 = new Musteri();
+                musteri2.Id = 2;
+                musteri2.isim = "Rabia";
+                musteri2.soyisim = "Kantar";
+                musteri2.yas = 45;
+
+                Musteri musteri3 = new Musteri();
+                musteri3.Id = 3;
+                musteri3.isim = "Ali";
+                musteri3.soyisim = "Akşam";
+                musteri3.yas = 44;
+
+                Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+                MusteriManager musteriManager = new MusteriManager();
+
+                musteriManager.Listele(musteriler);
+
+                musteriManager.Ekle(musteri2);
+
+                musteriManager.Sil(musteri3);
+
+            }
+
+        }
+    }
+
